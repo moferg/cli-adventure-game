dd29036765c43934b1f3304877b46d60e4973731
Marshall Ferguson <moferg01@louisville.edu>
2020-06-24 08:32:58 -0400

    initial commit

---
+++ cli_adventure_game.py
@@ -0,0 +1,6 @@
+# CLI Adventure Game - Marshall Ferguson - 6/2020
+
+print("Welcome to the Command Line Adventure Game!")
+
+name = input("Please enter your character's name:     ")
+print("Hello {}!".format(name))
