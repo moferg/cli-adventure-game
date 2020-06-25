0de8cc7fafda6e969aa0c0c74530577e25fc5942
Marshall Ferguson <moferg01@louisville.edu>
2020-06-25 11:50:00 -0400

    added some TODOs

--- cli_adventure_game.py
+++ cli_adventure_game.py
@@ -4,3 +4,17 @@ print("Welcome to the Command Line Adventure Game!")
 
 name = input("Please enter your character's name:     ")
 print("Hello {}!".format(name))
+
+# TODO - create a shop the player can buy things from and sell things to
+    # Player will be able to pick buy things back if needed
+    # Player will be able to regenerate the shop inventory if needed
+
+# TODO - create a wilderness area that the player can explore
+    # Player will be able to battle monsters to grind for xp/gold
+    # Player will be able to unlock different areas of the wilderness
+        # Player will have option of which part of wilderness to explore
+
+# TODO - create an area where the player can battle other fighters
+    # Player will be able to test themselves by battling fighters of increasing difficulty
+    # Player will be able to battle warriors, archers, and mages
+        # Player will not be able to choose between the three
