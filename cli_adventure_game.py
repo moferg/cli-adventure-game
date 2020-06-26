b22ef5ca8b3be177f629c32cca923959d2150012
Marshall Ferguson <moferg01@louisville.edu>
2020-06-26 16:55:04 -0400

    started rough draft of shop

--- cli_adventure_game.py
+++ cli_adventure_game.py
@@ -3,11 +3,29 @@
 print("Welcome to the Command Line Adventure Game!")
 
 name = input("Please enter your character's name:     ")
+name = name.title()
 print("Hello {}!".format(name))
 
 # TODO - create a shop the player can buy things from and sell things to
     # Player will be able to pick buy things back if needed
     # Player will be able to regenerate the shop inventory if needed
+
+shop_inventory = ["sword", "axe", "shield"]
+player_inventory = []
+
+print("Let's go to the shop!")
+print("Here is the shopkeepers inventory:")
+for x in shop_inventory:
+    print(x.title())
+purchase_answer = input("What would you like to purchase?     ")
+if purchase_answer == "sword":
+    print("A sword costs 50 gold.")
+elif purchase_answer == "axe":
+    print("An axe costs 50 gold.")
+elif purchase_answer == "shield":
+    print("A shield costs 50 gold.")
+else:
+    print("Oops! That wasn't a valid answer!")
 
 # TODO - create a wilderness area that the player can explore
     # Player will be able to battle monsters to grind for xp/gold
