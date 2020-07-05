711baa03639b96cb90164f417039928730080726
Marshall Ferguson <moferg01@louisville.edu>
2020-07-05 05:47:40 -0400

    added Character class

--- Character.py
+++ Character.py
@@ -1,1 +1,7 @@
 # CLI Adventure Game - Marshall Ferguson - 6/2020
+
+class Character:
+    def __init__(self, name, level, equipped):
+        self.name = name
+        self.level = level
+        self.equipped = equipped
