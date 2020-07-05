6989549792392e45957a44cf60c082c39b137c41
Marshall Ferguson <moferg01@louisville.edu>
2020-07-05 05:57:39 -0400

    added Player class

--- Player.py
+++ Player.py
@@ -1,1 +1,8 @@
 # CLI Adventure Game - Marshall Ferguson - 6/2020
+
+from Character import Character
+
+class Player(Character):
+    def __init__(self, gold, inventory):
+        self.gold = gold
+        self.inventory = inventory
