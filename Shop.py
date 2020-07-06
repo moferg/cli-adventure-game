e875ad5f068eab28fa9410e3b7d238e0999b2e7c
Marshall Ferguson <moferg01@louisville.edu>
2020-07-06 08:48:38 -0400

    added Shop class

--- Shop.py
+++ Shop.py
@@ -1,1 +1,7 @@
 # CLI Adventure Game - Marshall Ferguson - 6/2020
+
+class Shop:
+    def __init__(self, type_of_shop, gold, inventory):
+        self.type_of_shop = type_of_shop
+        self.gold = gold
+        self.inventory = inventory
